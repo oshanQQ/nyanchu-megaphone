594083cb2aab7ae54ebc9158ed9ce45924791384
Sotaro Tommykawa <tommysouta@gmail.com>
2021-01-15 01:24:52 +0900

    ♻ Refactor text field component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,24 +4,22 @@ export default function Home() {
   return (
     <main className="antialiased sans-serif bg-gray-200 container mx-auto">
       <Title>ニャンちゅうメガホン</Title>
-        />
+      <TextField
+        label="テキストを入力するに゛ゃ゛"
+        placeholder="み゛ゃ゛ーの金言だに゛ゃ゛"
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+      />
       </div>
 
       <div className="text-center">
         <button className="w-60 focus:outline-none border border-transparent py-2 px-5 rounded-lg shadow-md text-center text-gray bg-gray-50 hover:bg-gray-100 font-medium">
           変換に゛ゃ゛
-        </button>
-      </div>
-
-      <div className="mb-5 center">
-        <label htmlFor="output" className="font-bold mb-1 text-gray-700 block">
-          出゛力゛だ゛に゛ゃ゛ぁ゛
-        </label>
-        <textarea
-          id="output"
-          className="form-textarea w-full px-4 py-3 h-24 test-center rounded-lg shadow-md focus:outline-none focus:ring-2 resize-none focus:shadow-outline text-gray-600 font-medium"
-          placeholder="お゛お゛お゛お゛お゛ん゛"
-        />
+      <TextField
+        label="出゛力゛だ゛に゛ゃ゛ぁ゛"
+        placeholder="お゛お゛お゛お゛お゛ん゛"
+        value={outputValue}
+      />
     </main>
   );
 }
