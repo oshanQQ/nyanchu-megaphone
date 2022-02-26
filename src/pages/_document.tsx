cbec23389a06d7a8d8db4a8e44b76ff7a13aa52c
oshanQQ <oshanqq@gmail.com>
2022-02-26 23:11:30 +0900

    🎨 Styled component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,30 @@
-import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-interface Props {}
-class Document extends NextDocument<Props> {
-  render() {
-    return (
-      <Html lang="ja">
-        <Head></Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
-
-export default Document;
