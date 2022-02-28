65249943e4b10f1d49f30ae73e9cb18ae37381a2
oshanQQ <oshanqq@gmail.com>
2022-03-01 01:16:57 +0900

    🎨 Make the src directory

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import "../styles/globals.css";
+import "src/styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return <Component {...pageProps} />;
