6fe9736e431ba0e14d91bb377b2614543f5d9eaf
oshanQQ <oshanqq@gmail.com>
2022-03-01 01:32:10 +0900

    Revert "🎨 Make the src directory"
    This reverts commit 65249943e4b10f1d49f30ae73e9cb18ae37381a2.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import "src/styles/globals.css";
+import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return <Component {...pageProps} />;
