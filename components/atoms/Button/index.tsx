2192d1b556a3b5aad9b559b7300d75a8edb5568e
oshanQQ <oshanqq@gmail.com>
2022-03-03 23:33:26 +0900

    ✨ Add text form

--- components/atoms/Button/index.tsx
+++ components/atoms/Button/index.tsx
@@ -1,9 +1,20 @@
-type Props = {
-  children: string;
+import { css } from "@emotion/react";
+
+const form = css`
+  width: 1000px;
+  height: 180px;
+  padding: 20px;
+  font-size: x-large;
+  border-radius: 50px;
+  border-color: #4b82fe;
+  border-width: 4px;
+  background-color: white;
+  resize: none;
+  outline: none;
+`;
+
+const Form: React.VFC = () => {
+  return <textarea css={form}></textarea>;
 };
 
-const Button: React.VFC<Props> = ({ children }: Props) => {
-  return <button>{children}</button>;
-};
-
-export default Button;
+export default Form;
